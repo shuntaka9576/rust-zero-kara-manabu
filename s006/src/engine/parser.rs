309f6d36579528b25ea4fce325edf88bb91defd0
shuntaka9576 <shuntaka9576@gmail.com>
2023-10-04 05:08:21 +0900

    20231004

---
+++ s006/src/engine/parser.rs
@@ -0,0 +1,22 @@
+//! 正規表現の式をパースし、抽象構文木に変換
+use std::{
+    error::Error,
+    fmt::{self, Display},
+    mem::take,
+};
+
+#[derive(Debug)]
+pub enum AST {
+    Char(char),
+    Plus(Box<AST>),
+    Star(Box<AST>),
+    Question(Box<AST>),
+    Or(Box<AST>, Box<AST>),
+    Seq(Vec<AST>),
+}
+
+// fn take() {
+//     let mut n = Some(10);
+//     let v = take(&mut n);
+//     println("n = {:?}, v = {:?}", n, v);
+// }
