3a585b47c6c40e9c655c7f954125dfe7c3f207e2
shuntaka9576 <shuntaka9576@gmail.com>
2023-10-09 15:30:32 +0900

    20231009-add-test-panic

--- s006/src/main.rs
+++ s006/src/main.rs
@@ -8,6 +8,46 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+#[cfg(test)]
+mod tests {
+    use crate::{
+        engine::do_matching,
+        helper::{safe_add, SafeAdd},
+    };
+
+    #[test]
+    fn test_safe_add() {
+        let n: usize = 10;
+        assert_eq!(Some(30), n.safe_add(&20));
+
+        let n: usize = !0; // 2^64 -1
+        assert_eq!(None, n.safe_add(&1));
+
+        let mut n: usize = 10;
+        assert!(safe_add(&mut n, &20, || ()).is_ok());
+
+        let mut n: usize = !0;
+        assert!(safe_add(&mut n, &20, || ()).is_err());
+    }
+
+    #[test]
+    fn test_matching() {
+        assert!(do_matching("+b", "bbb", true).is_err());
+        assert!(do_matching("*b", "bbb", true).is_err());
+        assert!(do_matching("|b", "bbb", true).is_err());
+        assert!(do_matching("?b", "bbb", true).is_err());
+
+        assert!(do_matching("abc|def", "def", true).unwrap());
+        // assert!(do_matching("(abc)*", "abcabc", true).unwrap());
+        // assert!(do_matching("(ab|cd)+", "abcdcd", true).unwrap());
+        // assert!(do_matching("abc?", "ab", true).unwrap());
+
+        // assert!(!do_matching("abc|def", "efa", true).unwrap());
+        // assert!(!do_matching("(ab|cd)+", "", true).unwrap());
+        // assert!(!do_matching("abc?", "acb", true).unwrap());
+    }
+}
+
 fn main() -> Result<(), DynError> {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 2 {
