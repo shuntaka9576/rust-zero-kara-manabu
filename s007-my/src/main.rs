fc4d6ffe4d4e3603cf6cf70af9d03adaf8322acd
shuntaka9576 <shuntaka9576@gmail.com>
2023-10-16 05:11:21 +0900

    20231016

--- s007-my/src/main.rs
+++ s007-my/src/main.rs
@@ -1,8 +1,36 @@
-use rustyline::Editor;
+use rustyline::{error::ReadlineError, Editor};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut rl = Editor::<()>::new()?;
 
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.")
+    }
+
+    loop {
+        let readline = rl.readline(">> ");
+
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    rl.save_history("history.txt")?;
     Ok(())
 }
