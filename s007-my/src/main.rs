3442fabf30953cd9c68919c323d7c976eafff414
shuntaka9576 <shuntaka9576@gmail.com>
2023-10-18 07:00:31 +0900

    20231018

--- s007-my/src/main.rs
+++ s007-my/src/main.rs
@@ -1,20 +1,43 @@
+use nix::libc::{SIGCHLD, SIGINT, SIGTSTP};
 use rustyline::{error::ReadlineError, Editor};
-use std::error::Error;
+use signal_hook::iterator::Signals;
+use std::{
+    error::Error,
+    sync::mpsc::{channel, sync_channel, Sender},
+    thread,
+};
 
-fn main() -> Result<(), Box<dyn Error>> {
+pub type DynError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+enum WorkerMsg {
+    Signal(i32), // シグナル入力
+    Cmd(String), // コマンド入力
+}
+
+fn main() -> Result<(), DynError> {
     let mut rl = Editor::<()>::new()?;
 
     if rl.load_history("history.txt").is_err() {
         println!("No previous history.")
     }
 
+    let (worker_tx, worker_rx) = channel();
+
+    // SIGINIT, SIGTSTP, SIGCHILDがきたら、worker_rxに飛ばすスレッドを起動
+    spawn_sig_handler(worker_tx.clone())?;
+
+    // TODO recipeverの処理だけ書く(worker::newのspawnはリッチだからまず受け取るだけのものを書く)
+
+    let line = "aaa".to_string();
+
     loop {
         let readline = rl.readline(">> ");
 
         match readline {
             Ok(line) => {
-                rl.add_history_entry(line.as_str());
-                println!("Line: {}", line);
+                worker_tx.send(WorkerMsg::Cmd(line)).unwrap();
+                // rl.add_history_entry(line.as_str());
+                // println!("Line: {}", line);
             }
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
@@ -32,5 +55,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     rl.save_history("history.txt")?;
+    Ok(())
+}
+
+fn spawn_sig_handler(tx: Sender<WorkerMsg>) -> Result<(), DynError> {
+    let mut signals = Signals::new(&[SIGINT, SIGTSTP, SIGCHLD])?;
+
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            tx.send(WorkerMsg::Signal(sig)).unwrap()
+        }
+    });
+
     Ok(())
 }
